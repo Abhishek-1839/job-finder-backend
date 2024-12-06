760b784f889a5b8a296709f0841023947c124416
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-12-07 01:03:52 +0530

    CORSS

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ const cors = require("cors");
 dotenv.config();
 const PORT = 3000;
 const MONGO_URI = process.env.MONGO_URI;
-const corsOptions = {
-    origin: "https://storied-marzipan-b0aa35.netlify.app",
-    // origin : "http://localhost:5173",
-    methods: ["GET", "PUT", "DELETE", "POST", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  app.use(cors(corsOptions));
+
+app.use(cors({
+    // origin: 'https://<your-netlify-domain>.netlify.app',
+    origin: 'https://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true, // If you need to send cookies or authentication headers
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose
@@ -26,9 +24,9 @@ mongoose
     .catch((err) => console.error("MongoDB connection error:", err));
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
 app.use("/api/user", userRoutes);
 app.use("/api/job", jobRoutes);
 app.use((err, req, res, next) => {
